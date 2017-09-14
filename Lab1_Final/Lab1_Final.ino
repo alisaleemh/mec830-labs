a52553836efb4b77bd107d8f36a0b230e99c4093
T. Ansari <talhairfan17@gmail.com>
2017-09-14 16:40:34 -0400

    Final Codes for Lab1
    These changes worked as it was stated in the Lab Manual. 
    Stops at intersection and Beeps for 3secs and continues line following after left centre bumper is pushed.

---
+++ Lab1_Final/Lab1_Final.ino
@@ -0,0 +1,75 @@
+#include <mechbotShield.h>
+#include <USART.h>
+#include <avr/io.h>
+
+int main (void){
+
+    //ADC analog sensors
+    DDRD &= ~(1<<PD3);    
+    PORTD |= 0b00000000;
+
+    //setting bumper button as input
+    DDRD &= ~(1<<PD4); 
+    PORTD |= (1<<PD4);
+
+    //setting motors as output
+    DDRB |= ((1<<PB1)|(1<<PB2));   
+    PORTB &= ~((1<<PB1)|(1<<PB2));  
+  
+    initSoftSerial();
+    initMotor();
+    setLCDBackLight(255);
+    moveLCDCursor(0);
+    initADC();
+
+    int vel =750, tvel = 700, lsen_hi = 800, lsen_low = 600;
+        
+while(1){
+   
+   clrLCD();
+    
+   while (analog(1)>lsen_hi && analog(2)>lsen_hi){
+      motor(vel,vel);
+      moveLCDCursor(0);
+      lcdPrint("Straight");
+   }
+    
+   clrLCD();
+    
+   if (analog(0)>lsen_hi && analog(1)>lsen_hi){
+      
+      motor(0,tvel);
+      moveLCDCursor(0);
+      lcdPrint("Left");
+   
+   }
+    
+   clrLCD();
+  
+   if (analog(2)>lsen_hi && analog(3)>lsen_hi){
+      
+      motor(tvel,0);
+      moveLCDCursor(0);
+      lcdPrint("Right");
+   }   
+   
+   clrLCD();
+       
+   if (analog(3)>lsen_hi && analog(0)>lsen_hi){
+      
+      motor(0,0);
+      beep(3000,3000000);
+    
+      if ((PIND & (1<<PD4)) == 0){
+      
+         clrLCD();
+                
+         motor(1000,1000);
+         moveLCDCursor(0);
+         lcdPrint("Straight");
+               
+      }
+   }
+}
+}
+
