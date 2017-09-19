aff206091d24343bbb02cb61c02c49d52f164715
alisaleemh <ahussain@ca.ibm.com>
2017-09-19 17:43:32 -0400

    lab2code

---
+++ lab2/examples/mechbot_analog/mechbot_analog.ino
@@ -0,0 +1,28 @@
+#include <mechbotShield.h>
+#include <avr/io.h>
+
+int main(void)   {
+  initSoftSerial();
+  setLCDBackLight(147);
+  clrLCD();
+  moveLCDCursor(0);
+  
+  initADC();
+
+  uint16_t  value;
+
+  while (1)  {
+    value = analog(4);
+    
+    lcdPrint("Channel 4=");
+    lcdPrintDec(value);
+    delay_ms(500);
+    clrLCD();
+    moveLCDCursor(0);
+ 
+  }
+
+  
+}
+
+
---
+++ lab2/lab2Code/lab2Code.ino
@@ -0,0 +1,100 @@
+
+#include <Wire.h>
+#include <EVShield.h>
+
+
+
+
+void setup()   {
+
+
+  //declare EVShield as evshield
+  EVShield          evshield(0x34,0x36);
+
+  //set up hardware protocol to the EVshield
+  evshield.init( SH_HardwareI2C );
+
+  // reset motors.
+  evshield.bank_a.motorReset();
+
+  evshield.bank_a.motorRunUnlimited(SH_Motor_1,
+    SH_Direction_Forward,
+    SH_Speed_Slow
+  );
+  delay(1000);
+  evshield.bank_a.motorStop(SH_Motor_1, SH_Next_Action_Float);
+}
+
+void loop(){
+
+  if ( evshield.getButtonState(BTN_LEFT) == true ) {
+
+    leftCan();
+
+  }
+  if ( evshield.getButtonState(BTN_RIGHT) == true ) {
+
+    rightCan();
+  }
+
+  delay(1000);
+  evshield.ledSetRGB(0,0,0);
+
+
+}
+
+void leftCan() {
+  evshield.bank_a.motorRunDegrees(SH_Motor_1,
+    SH_Direction_Forward,
+    SH_Speed_Medium, 90, SH_Completion_Wait_For,
+    SH_Next_Action_Brake
+  );
+
+  delay(1000);
+
+
+  evshield.bank_a.motorRunRotations(SH_Motor_2,
+    SH_Direction_Reverse,
+    SH_Speed_Medium,
+    8,
+    SH_Completion_Wait_For,
+    SH_Next_Action_BrakeHold
+  );
+
+  delay (1000);
+
+  evshield.bank_a.motorRunDegrees(SH_Motor_1,
+    SH_Direction_Reverse,
+    SH_Speed_Medium, 90, SH_Completion_Wait_For,
+    SH_Next_Action_Brake
+  );
+}
+
+  void rightCan() {
+
+    evshield.bank_a.motorRunDegrees(SH_Motor_1,
+      SH_Direction_Reverse,
+      SH_Speed_Medium, 90, SH_Completion_Wait_For,
+      SH_Next_Action_Brake
+    );
+
+    delay(1000);
+
+
+    evshield.bank_a.motorRunRotations(SH_Motor_2,
+      SH_Direction_Forward,
+      SH_Speed_Medium,
+      8,
+      SH_Completion_Wait_For,
+      SH_Next_Action_BrakeHold
+    );
+
+    delay (1000);
+
+    evshield.bank_a.motorRunDegrees(SH_Motor_1,
+      SH_Direction_Forward,
+      SH_Speed_Medium, 90, SH_Completion_Wait_For,
+      SH_Next_Action_Brake
+    );
+
+  }
