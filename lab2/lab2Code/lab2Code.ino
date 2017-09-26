416f0c08e1302c7de0e6e02183b76f64dcd44b5a
alisaleemh <ahussain@ca.ibm.com>
2017-09-26 15:15:05 -0400

    lab3 code

--- lab2/lab2Code/lab2Code.ino
+++ lab2/lab2Code/lab2Code.ino
@@ -3,13 +3,90 @@
 #include <EVShield.h>
 
 
+  //declare EVShield as evshield
+  EVShield          evshield(0x34,0x36);
 
+  
+void leftCan() {
+
+    evshield.bank_a.motorRunDegrees(SH_Motor_2,
+    SH_Direction_Reverse,
+    SH_Speed_Slow, 90, SH_Completion_Wait_For,
+    SH_Next_Action_Brake
+  );
+    delay(1000);
+
+  evshield.bank_a.motorRunDegrees(SH_Motor_1,
+    SH_Direction_Forward,
+    SH_Speed_Medium, 90, SH_Completion_Wait_For,
+    SH_Next_Action_Brake
+  );
+
+
+
+
+
+  delay(1000);
+
+    evshield.bank_a.motorRunDegrees(SH_Motor_2,
+    SH_Direction_Forward,
+    SH_Speed_Slow, 120, SH_Completion_Wait_For,
+    SH_Next_Action_Brake
+  );
+
+
+
+  delay (1000);
+
+  evshield.bank_a.motorRunDegrees(SH_Motor_1,
+    SH_Direction_Reverse,
+    SH_Speed_Slow, 90, SH_Completion_Wait_For,
+    SH_Next_Action_Brake
+  );
+}
+
+  void rightCan() {
+
+      evshield.bank_a.motorRunDegrees(SH_Motor_2,
+    SH_Direction_Reverse,
+    SH_Speed_Slow, 90, SH_Completion_Wait_For,
+    SH_Next_Action_Brake
+  );
+
+      delay(1000);
+
+
+    evshield.bank_a.motorRunDegrees(SH_Motor_1,
+      SH_Direction_Reverse,
+      SH_Speed_Slow, 90, SH_Completion_Wait_For,
+      SH_Next_Action_Brake
+    );
+
+
+
+
+
+  delay(1000);
+
+    evshield.bank_a.motorRunDegrees(SH_Motor_2,
+    SH_Direction_Forward,
+    SH_Speed_Slow, 90, SH_Completion_Wait_For,
+    SH_Next_Action_Brake
+  );
+
+    delay (1000);
+
+    evshield.bank_a.motorRunDegrees(SH_Motor_1,
+      SH_Direction_Forward,
+      SH_Speed_Slow, 90, SH_Completion_Wait_For,
+      SH_Next_Action_Brake
+    );
+
+  }
 
 void setup()   {
 
 
-  //declare EVShield as evshield
-  EVShield          evshield(0x34,0x36);
 
   //set up hardware protocol to the EVshield
   evshield.init( SH_HardwareI2C );
@@ -17,12 +94,7 @@ void setup()   {
   // reset motors.
   evshield.bank_a.motorReset();
 
-  evshield.bank_a.motorRunUnlimited(SH_Motor_1,
-    SH_Direction_Forward,
-    SH_Speed_Slow
-  );
-  delay(1000);
-  evshield.bank_a.motorStop(SH_Motor_1, SH_Next_Action_Float);
+
 }
 
 void loop(){
@@ -43,58 +115,4 @@ void loop(){
 
 }
 
-void leftCan() {
-  evshield.bank_a.motorRunDegrees(SH_Motor_1,
-    SH_Direction_Forward,
-    SH_Speed_Medium, 90, SH_Completion_Wait_For,
-    SH_Next_Action_Brake
-  );
 
-  delay(1000);
-
-
-  evshield.bank_a.motorRunRotations(SH_Motor_2,
-    SH_Direction_Reverse,
-    SH_Speed_Medium,
-    8,
-    SH_Completion_Wait_For,
-    SH_Next_Action_BrakeHold
-  );
-
-  delay (1000);
-
-  evshield.bank_a.motorRunDegrees(SH_Motor_1,
-    SH_Direction_Reverse,
-    SH_Speed_Medium, 90, SH_Completion_Wait_For,
-    SH_Next_Action_Brake
-  );
-}
-
-  void rightCan() {
-
-    evshield.bank_a.motorRunDegrees(SH_Motor_1,
-      SH_Direction_Reverse,
-      SH_Speed_Medium, 90, SH_Completion_Wait_For,
-      SH_Next_Action_Brake
-    );
-
-    delay(1000);
-
-
-    evshield.bank_a.motorRunRotations(SH_Motor_2,
-      SH_Direction_Forward,
-      SH_Speed_Medium,
-      8,
-      SH_Completion_Wait_For,
-      SH_Next_Action_BrakeHold
-    );
-
-    delay (1000);
-
-    evshield.bank_a.motorRunDegrees(SH_Motor_1,
-      SH_Direction_Forward,
-      SH_Speed_Medium, 90, SH_Completion_Wait_For,
-      SH_Next_Action_Brake
-    );
-
-  }
---
+++ lab3/lab3.ino
@@ -0,0 +1,62 @@
+
+#include <Wire.h>
+#include <EVShield.h>
+#include <USART.h>
+
+EVShield          evshield(0x34,0x36);
+int encoderBefore, encoderAfter;
+
+
+
+void setup()   {
+
+
+  //declare EVShield as evshield
+
+  //  set up hardware protocol to the EVshield
+  evshield.init( SH_HardwareI2C );
+
+  // reset motors.
+  evshield.bank_a.motorReset();
+  Serial.begin(9600);
+
+  Serial.print("^Time before \n");
+  Serial.print(millis());
+  encoderBefore = evshield.bank_a.motorGetEncoderPosition(SH_Motor_1);
+  Serial.print("Encoder Position Before\n");
+  Serial.print(encoderBefore);
+
+
+
+  Serial.print("^Time After Motor\n");
+  Serial.print(millis());
+  encoderAfter = evshield.bank_a.motorGetEncoderPosition(SH_Motor_1);
+  Serial.print("\nEncoder Position Before\n");
+  Serial.print(encoderAfter);
+
+
+}
+
+void loop(){
+
+
+  if ( evshield.getButtonState(BTN_LEFT) == true ) {
+
+    evshield.bank_a.motorRunUnlimited(SH_Motor_1,
+      SH_Direction_Reverse,
+      SH_Speed_Slow
+    );
+
+  }
+  if ( evshield.getButtonState(BTN_RIGHT) == true ) {
+
+    evshield.bank_a.motorRunDegrees(SH_Motor_1,
+      SH_Direction_Forward,
+      SH_Speed_Medium, 90, SH_Completion_Wait_For,
+      SH_Next_Action_Brake
+    );    }
+
+   delay(1000);
+
+
+  }
