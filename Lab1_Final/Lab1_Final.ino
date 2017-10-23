0bc9af3239d459559535c131dc9a912163213b2b
alisaleemh <ahussain@ca.ibm.com>
2017-10-23 16:35:20 -0400

    streamlined lab1 linefollowing code

--- Lab1_Final/Lab1_Final.ino
+++ Lab1_Final/Lab1_Final.ino
@@ -2,6 +2,14 @@
 #include <USART.h>
 #include <avr/io.h>
 
+int vel =750, tvel = 700, lsen_hi = 800, lsen_low = 600;
+
+void moveStraight() ;
+void turnRight();
+void turnLeft();
+
+  
+
 int main (void){
 
     //ADC analog sensors
@@ -22,54 +30,63 @@ int main (void){
     moveLCDCursor(0);
     initADC();
 
-    int vel =750, tvel = 700, lsen_hi = 800, lsen_low = 600;
+
+
         
 while(1){
    
    clrLCD();
     
    while (analog(1)>lsen_hi && analog(2)>lsen_hi){
-      motor(vel,vel);
-      moveLCDCursor(0);
-      lcdPrint("Straight");
+    moveStraight();
    }
     
    clrLCD();
     
    if (analog(0)>lsen_hi && analog(1)>lsen_hi){
-      
-      motor(0,tvel);
-      moveLCDCursor(0);
-      lcdPrint("Left");
+    turnLeft();
    
    }
     
    clrLCD();
   
    if (analog(2)>lsen_hi && analog(3)>lsen_hi){
-      
-      motor(tvel,0);
-      moveLCDCursor(0);
-      lcdPrint("Right");
+    turnRight();  
    }   
    
    clrLCD();
        
    if (analog(3)>lsen_hi && analog(0)>lsen_hi){
-      
-      motor(0,0);
-      beep(3000,3000000);
-    
-      if ((PIND & (1<<PD4)) == 0){
-      
-         clrLCD();
-                
-         motor(1000,1000);
-         moveLCDCursor(0);
-         lcdPrint("Straight");
-               
-      }
+    stopIntersection();
    }
 }
 }
+
+void stopIntersection() {
+  motor(0,0);
+  beep(3000,3000000);
+  moveLCDCursor(0);
+  lcdPrint("Stopped");
+}
+
+void moveStraight () {
+    motor(vel,vel);
+    moveLCDCursor(0);
+    lcdPrint("Straight");
+    return NULL;
+}
+
+void turnRight () {
+      motor(tvel,0);
+      moveLCDCursor(0);
+      lcdPrint("Right");
+      return NULL;
+}
+void turnLeft () {
+      motor(0,tvel);
+      moveLCDCursor(0);
+      lcdPrint("Left");
+      return NULL;
+}
+
 
--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # mec830-labs
 
 
-### Calibration Values - Line Sensors - MECHBOT 6
+### Calibration Values - Line Sensors - MECHBOT 13
 
 
 | Tables        | Left          | Left Center  | Right Center | Right  |
 | ------------- |:-------------:| -----:       | ------------ | ------ |
-| On line       | 800           | 800          |  800         | 900    |
-| Not On line   | 480           | 400          |  300         | 400    |
-| Edge          | 700           | 650          |  500         | 700    |
+| On line       | 930           | 885          |  850         | 930    |
+| Not On line   | 580           | 430          |  300         | 630    |
+| Edge          | 750           | 630          |  600         | 730    |
 
 
